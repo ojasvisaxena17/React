c852d8552a42081ba4061835992520e0aaf80acb
shivankar <yashsrvstv1998@gmail.com>
2025-12-26 21:54:45 +0530

    created react app

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+   <h1>
+    Hello, Vite!
+    I am Ojasvi!
+    Today is December 26, 2025.
+    Today i created my first react app using vite. Firstly,
+    I created a folder under documents named React.
+    Then I opened vs code terminal and ran npm create vite@latest.
+    After creation of react project by selecting library React and language javascript,
+    I ran cd vite-project to go inside the folder and run the command npm install to install all the dependencies.
+    Finally, I ran npm run dev and my react app is successfully created which is visible on localhost 5173.
+   </h1>)};
+
+export default App;
